261216812fd9ffa4cc8cb55e0abe217cc88090d6
Musab Rayan <musab.r@prudent.ai>
2026-03-28 11:21:48 +0530

    feat: add todo app with models, views, and URL routing
    - Add todo Django app (models, views, urls, admin, migrations)
    - Update home.html template
    - Register todo app in settings and wire up URLs

---
+++ Todo/todo/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+from todo.models import Task
+
+# Create your views here.
+
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
